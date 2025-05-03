ba557c3d44b2636c0c58726360af2c16193fa035
caioRoldao <caio.roldaoti@gmail.com>
2025-05-03 08:48:16 -0300

    exericicos cadastro produto

---
+++ estudo-java-basico/src/CadastroProduto/Produto.java
@@ -0,0 +1,43 @@
+package CadastroProduto;
+
+public class Produto {
+
+    private String nome;
+    private Double preco;
+    private int quantidade;
+
+    public Produto(String nome,Double preco,int quantidade){
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void ExibirInformacoes(){
+        System.out.println("nome" + " " +nome);
+        System.out.println("preco " + preco);
+        System.out.println("quantidade " + quantidade);
+    }
+
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public Double getPreco(){
+        return preco;
+    }
+    public void setPreco(Double preco){
+        this.preco = preco;
+    }
+    public int getQuantidade(){
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+
+
+}
--- estudo-java-basico/src/CadastroProduto/main.java
+++ estudo-java-basico/src/CadastroProduto/main.java
@@ -1,4 +1,10 @@
 package CadastroProduto;
 
 public class main {
+    public static void main(String[] args) {
+
+        Produto produto = new Produto("notebook",2000.00, 20);
+        produto.ExibirInformacoes();
+
+    }
 }
